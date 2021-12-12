bd04bb0db79f7e1e6ae358ff50d93b7368aa53f5
hakirot <hackirot@proton.me>
2021-12-12 15:06:31 -0800

    iterations

--- gitConfs.sh
+++ gitConfs.sh
@@ -1,8 +1,13 @@
+# Run git commands from this folder, not folder where cmd was called
 cd "$(dirname "$0")"
 git add -A
+# Use all arguments for git commit msg
 str="'$*'"
 gitCmd="git commit -m ${str}"
+# Run gitCmd as shellcode for ${str}
 eval "$gitCmd"
+# Pull token
 value=`cat ~/Documents/notes/git.txt`
+# Execute push
 cmd="git push https://hakirot:${value}@github.com/hakirot/BackupConfigs.git"
 eval "$cmd"
